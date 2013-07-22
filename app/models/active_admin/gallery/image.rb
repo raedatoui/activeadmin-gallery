315ac859822a7e9d46cfe1716137e2d0443523f1
Raed Atoui <raed.atoui@gmail.com>
2013-07-22 19:51:56 -0400

    same stuff

--- app/models/active_admin/gallery/image.rb
+++ app/models/active_admin/gallery/image.rb
@@ -2,7 +2,7 @@ module ActiveAdmin::Gallery
   class Image < ActiveRecord::Base
     self.table_name = "active_admin_gallery_images"
     belongs_to :imageable, polymorphic: true
-    attr_accessible :alt, :title, :position
+    # attr_accessible :alt, :title, :position
 
     image_accessor :image
     # attr_accessible :image, :retained_image, :image_url, :remove_image
